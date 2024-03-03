b912c384e42f538de7017bb6155b157d90b2a80a
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-03-03 20:21:00 +0530

    Create Reverse LL in k groups.cpp

---
+++ Reverse LL in k groups.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node* nReverse(Node* head, int n,int b[],int i) {
+    
+    if(head == NULL) {
+        return NULL;
+    }
+    if(i>=n){
+        return head;
+    }
+    if(b[i]==0){
+        i++;
+        return nReverse(head,n,b,i);
+    }
+    
+    
+    Node* next = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count= 0;
+    
+    while( curr != NULL && count < b[i]) {
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+
+    if (next != NULL) {
+        head->next = nReverse(next, n,b,i+1);
+    }
+    return prev;
+    
+}
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+	// Write your code here.
+    int i = 0;
+	return nReverse(head, n,b,i);
+}
